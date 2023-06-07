448f93d473ff3eb80defe22f96540623307df4a8
Yubal <ydefuente@gmail.com>
2023-06-07 21:07:24 +0200

    probando camara trasera

--- Controllers/AssistantFormController.cs
+++ Controllers/AssistantFormController.cs
@@ -17,7 +17,7 @@ public class AssistantFormController : Controller
         _hostEnvironment = hostEnvironment;
     }
 
-    [Route("meethub/AssistantForm/Form/{id?}")]
+    [Route("AssistantForm/Form/{id?}")]
     public IActionResult Form(int id)
     {
         var evento = _context.Events.FirstOrDefault(e => e.IdEvent == id);
